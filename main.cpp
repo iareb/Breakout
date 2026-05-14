1632014097bd1390cc23728f6c42dc6dc38c34bf
iareb <lolluz6@gmail.com>
2026-05-14 17:18:39 +0200

    Added CMake configuration and main file with a basic application loop

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+int main(int argc, char** argv) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        CheckSDLError("SDL_Init");
+        return 1;
+    }
+
+    if (!TTF_Init()) {
+        CheckSDLError("TTF_Init");
+        return 1;
+    }
+
+    Uint64 LastTick{ SDL_GetPerformanceCounter() };
+    SDL_Event E;
+    while (true) {
+        while (SDL_PollEvent(&E)) {
+
+
+        }
+
+        Uint64 CurrentTick{ SDL_GetPerformanceCounter() };
+        float DeltaTime{
+          static_cast<float>(CurrentTick - LastTick) /
+            static_cast<float>(SDL_GetPerformanceFrequency())
+        };
+        LastTick = CurrentTick;
+
+    }
+
+    return 0;
+}
